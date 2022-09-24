2ba7caa5321a31bce4ac304418dca616d3019c65
Olivia McKee <olivia.mckee97@gmail.com>
2022-09-24 14:29:21 -0500

    recipe card, home and nomatch

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import NoMatch from './pages/NoMatch';
+import Home from './pages/Home';
+import Recipe from './components/Recipe';
+import RecipeList from './components/RecipeList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="*"
+            element={<NoMatch />}
+          />
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
