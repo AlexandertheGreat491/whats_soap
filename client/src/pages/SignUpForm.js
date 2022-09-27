8f1d954eb536ae7e1484e72e034ab811e19ea121
agvan <agvandyke25@gmail.com>
2022-09-27 11:50:06 -0500

    made LoginForm and SignUpForm pages instead of components, mutations still broken

---
+++ client/src/pages/SignUpForm.js
@@ -0,0 +1,92 @@
+// imports React along with the useState & useEffect hooks
+// add in useEffect hook
+import React, { useState} from 'react';
+// imports the Form, Button, & Alert components from react-bootstrap
+//import { Form, Button, Alert } from 'react-bootstrap';
+// imports useMutation
+//import {useMutation} from '@apollo/client';
+// imports the ADD_USER mutation
+//import { ADD_USER } from '../utils/mutations';
+
+// imports the auth logic
+//import Auth from '../utils/auth';
+
+const SignUpForm = () => {
+  // sets the initial form state
+  const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+
+  //const [addUser, {error}] = useMutation(ADD_USER);
+
+   
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({ ...formState, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // checks if form has everything (as per react-bootstrap docs)
+    //const form = event.currentTarget;
+    //if (form.checkValidity() === false) {
+      //event.preventDefault();
+      //event.stopPropagation();
+    //}
+
+     //try {
+       //const { data } = await addUser({ variables: { ...formState } });
+
+        //Auth.login(data.addUser.token);
+     //} catch (err) {
+       //console.error(err);
+     //}
+
+  };
+
+  return (
+    <main className='flex-row justify-center mb-4'>
+        <div className='col-12 col-md-6'>
+            <div className='card'>
+                <h4 className='card-header'>Sign Up</h4>
+                <div className='card-body'>
+                    <form onSubmit={handleFormSubmit}>
+                        <input
+                        className='form-input'
+                        placeholder='Your username'
+                        name="username"
+                        type="username"
+                        id="username"
+                        value={formState.username}
+                        onChange={handleChange}
+                        />
+                        <input
+                        className='form-input'
+                        placeholder='Your email'
+                        name='email'
+                        type='email'
+                        id='email'
+                        value={formState.email}
+                        onChange={handleChange}
+                        />
+                        <input
+                        className='form-input'
+                        placeholder='******'
+                        name='password'
+                        type='password'
+                        id='password'
+                        value={formState.password}
+                        onChange={handleChange}
+                        />
+                        <button className='btn d-block w-100' type='submit'>
+                            Submit
+                        </button>
+                    </form>
+                    {/*{error && <div>Signup failed</div>}*/}
+                </div>
+            </div>
+        </div>
+    </main>
+  );
+};
+// exports the SignUpForm function so that the SignupForm component can be used by the app
+export default SignUpForm;
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,43 +1,52 @@
 import decode from "jwt-decode";
 
 class AuthService {
+  // retrieve data saved in token
   getProfile() {
     return decode(this.getToken());
   }
 
+  // checks if the user is still logged in
   loggedIn() {
-    // checks if there is a saved token and it's valid
+    // checks if there is a saved toekn and it's still valid
     const token = this.getToken();
+    // uses type coersion to check if token isn't undefined and the token isn't expired
     return !!token && !this.isTokenExpired(token);
   }
 
+  // checks if the token has expired
   isTokenExpired(token) {
     try {
       const decoded = decode(token);
       if (decoded.exp < Date.now() / 1000) {
         return true;
-      } else return false;
+      } else {
+        return false;
+      }
     } catch (err) {
       return false;
     }
   }
 
+  // retrieves token from localStorage
   getToken() {
-    // retrieves the user token from localStorage
+    // retrieves the user from localStorage
     return localStorage.getItem("id_token");
   }
 
+  // sets token to localStorage and reloads the page to homepage
   login(idToken) {
     // saves user token to localStorage
-    localStorage.setItem('id_token', idToken);
+    localStorage.setItem("id_token", idToken);
 
-    window.location.assign('/');
+    window.location.assign("/");
   }
 
+  // clears token from localStorage and force logout with reload
   logout() {
-    // clears the user token & profile data from localStorage
+    // clears user token and profile data from localStorage
     localStorage.removeItem('id_token');
-    // reloads the page & reset the state of the application
+    // reloads the page and resets the state of the application
     window.location.assign('/');
   }
 }
